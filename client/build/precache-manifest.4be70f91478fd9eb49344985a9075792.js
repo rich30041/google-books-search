5b60df89f053462a9ae2e9081d62d9835f857da8
Matthews <212689101@ge.com>
2021-02-21 16:59:41 -0500

    deploying

---
+++ client/build/precache-manifest.4be70f91478fd9eb49344985a9075792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d08338d868b6a6fff306aedb33a04a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2031eb82d85e2528e8f9",
+    "url": "/static/css/main.0d3ba921.chunk.css"
+  },
+  {
+    "revision": "09d24a425dae0a3e7846",
+    "url": "/static/js/2.dacdc361.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/static/js/2.dacdc361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2031eb82d85e2528e8f9",
+    "url": "/static/js/main.73ea4e75.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "a56052d92c8d0b19fc2c048b8dbad61c",
+    "url": "/static/media/bookshelf.a56052d9.jpg"
+  }
+]);
